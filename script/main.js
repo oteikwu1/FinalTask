c5584b658f52482456e47099cd0ba1964a0323d1
godfrey <oteikwuon@gmail.com>
2025-08-29 11:03:27 +0100

    update all changes

--- script/main.js
+++ script/main.js
@@ -1,15 +1,5 @@
 //  GLOBAL VARIABLE
 
-// const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
-// const API_URL = 'https://fakestoreapi.com/products';
-// const API_BASE = 'https://fakestoreapi.com/products';
-// const PROXY_URL = 'https://corsproxy.io/?u=';
-
-// fetch(PROXY_URL + API_URL) 
-//   .then((response) => response.json())
-//   .then((data) => console.log(data))
-//   .catch((error) => console.error('Fetch error:', error));
-
 const API_BASE = 'https://fakestoreapi.com/products';
 
 fetch(API_BASE)
